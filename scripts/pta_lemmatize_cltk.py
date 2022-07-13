df916e3a2392d79ea637ac9ca20f7fa15377152d
Annette von Stockhausen <annette.von_stockhausen@bbaw.de>
2022-07-13 15:47:26 +0200

    Update script for lemmatization
    added latin

--- scripts/pta_lemmatize_cltk.py
+++ scripts/pta_lemmatize_cltk.py
@@ -1,20 +1,23 @@
 # %% [markdown]
 # # Lemmatize PTA data with CLTK (+ POS, + morphology)
+# 
+# for Greek and Latin texts
 
 # %% [markdown]
 # ## Functions
 
 # %%
 import os,glob,re
+from MyCapytain.resources.texts.local.capitains.cts import CapitainsCtsText
+from MyCapytain.common.constants import Mimetypes, XPATH_NAMESPACES
 import json
-import subprocess
 import collections
 import pandas as pd
 from copy import deepcopy
 from dataclasses import dataclass
 from boltons.cacheutils import cachedproperty
 from cltk import NLP
-#from cltk.alphabet import grc
+from cltk.alphabet import lat
 from cltk.core.data_types import Doc, Pipeline, Process
 from cltk.core.exceptions import CLTKException
 from cltk.stops.processes import StopsProcess
@@ -111,28 +114,30 @@ def tokenize(inputText):
 
 # %%
 def remove_interpunction(inputText):
-    return re.sub(r'[.,:··;()›»«‹⁘—><\[\]\+\-\n]+', r'', inputText) 
+    return re.sub(r'[.,:··;\?()›»«‹⁘—><\[\]\+\-\n]+', r'', inputText) 
 
 # %%
 def remove_numbering(inputText):
-    return re.sub(r'[0-9a-zA-Z]+', r' ', inputText) # also latin alphabet
+    return re.sub(r'[0-9]+', r' ', inputText) 
+
+# %%
+def remove_latin(inputText):
+    return re.sub(r'[a-zA-Z]+',r'', inputText)
 
 # %%
 def tei_xml_to_plaintext(file_path):
-    """Use Saxon and xslt to convert TEI to plaintext."""
-    # plaintext_path = ".".join(file_path.split("/")[-1].split(".")[:3])
-    stylesheet = '/home/stockhausen/Dokumente/Severian/xslt/plaintext.xsl'
-    saxon = '/home/stockhausen/Dokumente/projekte/pta_collator/vendor/saxon9he.jar'
-    buffer = subprocess.run(['java', '-jar', saxon, f'-s:{file_path}', f'-xsl:{stylesheet}'],
-                            stdout=subprocess.PIPE).stdout
-    plaintext = buffer.decode('utf-8')
-    # save plaintext
-    # with open('/home/stockhausen/Dokumente/projekte/pta_data/plaintext/'+plaintext_path+'.txt', 'w') as outfile:
-    #    outfile.write(plaintext)
+    plaintext = ''
+    with open(file_path) as file_open:
+        text = CapitainsCtsText(resource=file_open)
+        for ref in text.getReffs(level=len(text.citation)):
+            psg = text.getTextualNode(subreference=ref, simple=True)
+            psg.plaintext_string_join = "" 
+            text_line = psg.export(Mimetypes.PLAINTEXT, exclude=["tei:note","tei:rdg"])
+            plaintext += text_line
     return plaintext
 
 # %%
-def analyze_files(files_path):
+def analyze_grc_files(files_path):
     '''
     Load all files from files_path and analyze with CLTK,
     finally write out two dicts: 
@@ -143,7 +148,7 @@ def analyze_files(files_path):
     xml_dir = os.path.expanduser(files_path)
     xml_paths = glob.glob(xml_dir)
     grc_paths = [path for path in sorted(xml_paths) if 'grc' in path]
-    pta_dict = []
+    pta_grc_dict = []
     wordlemma_grc = []
     grc_pipeline_custom_1 = Pipeline(language="grc", description="", processes=[GreekTokenizationProcess, NormalisationProcess, GreekStanzaProcess, StopsProcess])
     cltk_nlp_grc = NLP(language="grc", custom_pipeline=grc_pipeline_custom_1, suppress_banner=True)
@@ -156,7 +161,7 @@ def analyze_files(files_path):
         text = tei_xml_to_plaintext(xml_path)
         text_lowered = text.lower() # Remove capitals
         #text_ana = grc.filter_non_greek(text_lowered) # leave only Greek letters, removes also Apostrophe -> not good
-        text_ana = remove_interpunction(remove_numbering(text_lowered))
+        text_ana = remove_latin(remove_interpunction(remove_numbering(text_lowered)))
         file_dict["urn"] = "urn:cts:pta:"+ptaid
         nlp = cltk_nlp_grc.analyze(text=text_ana)
         words = nlp.words
@@ -173,16 +178,77 @@ def analyze_files(files_path):
         lemmata_counted = collections.Counter(lemmata).most_common()
         #with open("/home/stockhausen/Dokumente/projekte/pta_data/plaintext/"+ptaid+".txt", "w") as text_file:
         #    text_file.write(remove_interpunction(text))
-        with open("/home/stockhausen/Dokumente/projekte/pta_data/plaintext_lemmatized/"+ptaid+".txt", "w") as text_file:
+        with open("/home/stockhausen/Dokumente/projekte/pta_data_plaintext/lemmatized/"+ptaid+".txt", "w") as text_file:
             text_file.write(" ".join(nlp.lemmata))
         file_dict["tokens"] = tokens_counted
         file_dict["lemmata"] = lemmata_counted
-        pta_dict.append(file_dict)
-    return pta_dict, wordlemma_grc 
+        pta_grc_dict.append(file_dict)
+    return pta_grc_dict, wordlemma_grc 
 
 
 # %%
-pta_dict, wordlemma_grc = analyze_files("/home/stockhausen/Downloads/pta_data/data/*/*/*.xml")
+def analyze_lat_files(files_path):
+    '''
+    Load all files from files_path and analyze with CLTK,
+    finally write out two dicts: 
+    - wordlemma_grc: all words in corpus and their analysis
+    - pta_dict: per URN info on words (counted, stopwords removed) and lemmata (counted, stopwords removed)
+    - also plaintext files of text and lemmatized text are written to pta_data repo. 
+    '''
+    xml_dir = os.path.expanduser(files_path)
+    xml_paths = glob.glob(xml_dir)
+    lat_paths = [path for path in sorted(xml_paths) if 'lat' in path]
+    pta_lat_dict = []
+    wordlemma_lat = []
+    cltk_nlp_lat = NLP(language="lat", suppress_banner=True)
+    cltk_nlp_lat.pipeline.processes.pop(-1)
+    print("Analysing...")
+    for xml_path in lat_paths:
+        file_dict = {}
+        short_path = "/".join(xml_path.split("/")[8:])
+        print(short_path)
+        ptaid = "".join(short_path).split(".xml")[0]
+        text = tei_xml_to_plaintext(xml_path)
+        text_lowered = text.lower() # Remove capitals
+        text_ana = lat.drop_latin_punctuation(text_lowered)
+        text_ana = lat.disappear_round_brackets(text_ana)
+        file_dict["urn"] = "urn:cts:pta:"+ptaid
+        nlp = cltk_nlp_lat.analyze(text=text_ana)
+        words = nlp.words
+        for entry in words:
+            if entry.upos !="PUNCT":
+                tokens = {}
+                tokens["Word"] = entry.string
+                tokens["Lemma"] = entry.lemma
+                tokens["POS"] = entry.upos
+                features = entry.features
+                tokens["Morphology"] = ', '.join(f'{k}: {v}' for k, v in features.items())
+                wordlemma_lat.append(tokens)
+        tokens_counted = collections.Counter(nlp.tokens_stops_filtered).most_common() #without stopwords
+        lemmata = [x.lemma for x in nlp.words if x.stop == False] # without stopwords
+        lemmata_counted = collections.Counter(lemmata).most_common()
+        #with open("/home/stockhausen/Dokumente/projekte/pta_data/plaintext/"+ptaid+".txt", "w") as text_file:
+        #    text_file.write(remove_interpunction(text))
+        #lemma_for_file = [x for x in nlp.lemmata if re.search(r'[^0-9.,()?!:;]',x)]
+        with open("/home/stockhausen/Dokumente/projekte/pta_data_plaintext/lemmatized/"+ptaid+".txt", "w") as text_file:
+            text_file.write(" ".join(nlp.lemmata))
+        file_dict["tokens"] = tokens_counted
+        file_dict["lemmata"] = lemmata_counted
+        pta_lat_dict.append(file_dict)
+    return pta_lat_dict, wordlemma_lat 
+
+# %%
+pta_grc_dict, wordlemma_grc = analyze_grc_files("/home/stockhausen/Downloads/pta_data/data/*/*/*.xml")
+
+# %%
+pta_lat_dict, wordlemma_lat = analyze_lat_files("/home/stockhausen/Downloads/pta_data/data/*/*/*.xml")
+
+# %%
+# Combine pta_grc_dict and pta_lat_dict 
+pta_dict = pta_grc_dict+pta_lat_dict
+
+# %% [markdown]
+# ## Save results
 
 # %%
 # Write analytical data to file
@@ -196,12 +262,17 @@ df = pd.DataFrame(wordlemma_grc)
 df.drop_duplicates(inplace=True)
 #df.columns = ["Word","Lemma","POS","Morphology"]
 #df.drop(['POS'], axis=1, inplace=True) # for the time being to be in concordance to prior format
-
-# %%
 #df
-
-# %%
 # Export dataframe to json
 df.to_json(r'/home/stockhausen/Dokumente/projekte/pta_lexika/wordlemma_grc_cltk.json', orient='records', force_ascii=False, indent=4)
+
+# %%
+df = pd.DataFrame(wordlemma_lat)
+df.drop_duplicates(inplace=True)
+#df.columns = ["Word","Lemma","POS","Morphology"]
+#df.drop(['POS'], axis=1, inplace=True) # for the time being to be in concordance to prior format
+#df
+# Export dataframe to json
+df.to_json(r'/home/stockhausen/Dokumente/projekte/pta_lexika/wordlemma_lat_cltk.json', orient='records', force_ascii=False, indent=4)
 
 
